cc9574acc6b4710d987e3160e294cff0089c008c
pedrobem <pedrohigino69@gmail.com>
2026-09-17 18:00:05 -0300

    Add: fruit-into-baskets - submission-2

---
+++ Data Structures & Algorithms/fruit-into-baskets/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        frequencies = {}
+
+        maxx = 0
+        left = 0
+        for right, fruit in enumerate(fruits):
+            if fruit not in frequencies:
+                frequencies[fruit] = 1
+            else:
+                frequencies[fruit] += 1
+
+            while len(frequencies) > 2:
+                frequencies[fruits[left]] -= 1
+                if frequencies[fruits[left]] == 0:
+                    del frequencies[fruits[left]]
+                left += 1
+
+            curr = right - left + 1
+            maxx = max(maxx, curr)
+
+        return maxx
