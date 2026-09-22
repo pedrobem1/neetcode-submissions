3b44417c3f6dc5bd6f189bb1c80a770ed7de878d
pedrobem <pedrohigino69@gmail.com>
2026-09-22 13:31:16 -0300

    Add: pascals-triangle - submission-0

---
+++ Data Structures & Algorithms/pascals-triangle/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def generate(self, numRows: int) -> list[list[int]]:
+        triangle = []
+
+        for row in range(numRows):
+            current_row = [1] * (row+1)
+            for col in range(1, row):
+                current_row[col] = (triangle[row-1][col-1] + triangle[row-1][col])
+            triangle.append(current_row)
+
+        return triangle
+        
